e27ed36fa618fd086db058dc2f56a53f4f99e7d7
Yui-Ezic <mishan221199@gmail.com>
2020-07-24 14:25:18 +0300

    Fixed path to dictionaries for correct work in UNIX system

--- config/banbuilder.php
+++ config/banbuilder.php
@@ -3,8 +3,8 @@
 return [
     'dictionaries' => [
         'rus' => [
-            'blacklist' => resource_path('dictionaries\rus\blacklist.php'),
-            'whitelist' => require resource_path('dictionaries\rus\whitelist.php')
+            'blacklist' => resource_path('dictionaries/rus/blacklist.php'),
+            'whitelist' => require resource_path('dictionaries/rus/whitelist.php')
         ]
     ]
 ];
