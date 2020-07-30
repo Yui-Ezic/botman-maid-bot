e11f58647b6048876d5d48edbce12585fe248a32
Yui-Ezic <mishan221199@gmail.com>
2020-07-30 17:07:27 +0300

    Installed twemoji

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ let mix = require('laravel-mix');
 mix.setPublicPath('public/build')
     .setResourceRoot('/build/')
     .js('resources/js/app.js', 'js')
-    .sass('resources/sass/app.scss', 'css');
+    .sass('resources/sass/app.scss', 'css')
+    .copy('node_modules/twemoji/dist/twemoji.min.js', 'public/build/js/twemoji.min.js');
